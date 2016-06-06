bc0d1358d1bcfdfa23a01961550df84f5f4f1344
onagorodniuk <gagarinq9@gmail.com>
2016-06-06 16:49:15 +0300

    multiple fixes

--- cabot_alert_stashboard/models.py
+++ cabot_alert_stashboard/models.py
@@ -19,7 +19,9 @@ sb_warn = env['STASHBOARD_WARNING']
 sb_error = env['STASHBOARD_ERROR']
 sb_crit = env['STASHBOARD_CRITICAL']
 
-sb_template = "Service {{ service.name }} {% if service.overall_status == service.PASSING_STATUS %}is back to normal{% else %}reporting {{ service.overall_status }} status{% endif %}."
+#sb_template = "Service {{ service.name }} {% if service.overall_status == service.PASSING_STATUS %}is back to normal{% else %}reporting {{ service.overall_status }} status{% endif %}."
+
+sb1_template = """Service {{ service.name }} {% if service.overall_status == service.PASSING_STATUS %}is back to normal{% else %} reporting {{ service.overall_status }} status{% endif %} {% if service.overall_status != service.PASSING_STATUS %}Checks failing: {% for check in service.all_failing_checks %}{% if check.check_category == 'Jenkins check' %}{% if check.last_result.error %}{{ check.name }} ({{ check.last_result.error|safe }}) {{jenkins_api}}job/{{ check.name }}/{{ check.last_result.job_number }}/console{% else %}{{ check.name }} {{jenkins_api}}/job/{{ check.name }}/{{check.last_result.job_number}}/console{% endif %}{% else %}{{ check.name }}{% if check.last_result.error %}({{ check.last_result.error|safe }}){% endif %}{% endif %}{% endfor %}{% endif %}{% if alert %}{% for alias in users %}@{{ alias }}{% endfor %}{% endif %}"""
 
 class StashboardAlert(AlertPlugin):
     name = "Stashboard"
@@ -58,7 +60,7 @@ class StashboardAlert(AlertPlugin):
             'scheme': settings.WWW_SCHEME,
             'jenkins_api': settings.JENKINS_API,
             })
-        message = Template(sb_template).render(c)
+        message = Template(sb1_template).render(c)
         self._send_stashboard_alert(message, key=str(service.id), priority=priority, service=service)
 
     def _send_stashboard_alert(self, message, key, priority, service):
@@ -67,29 +69,35 @@ class StashboardAlert(AlertPlugin):
         s.auth = OAuth1(u'anonymous', u'anonymous',
                 sb_token, sb_secret)
 
-        resp = s.get('%s/services/%s' % (sb_alert_url, service.name))
+        resp = s.get('%s/services/%s' % (sb_alert_url, key))
         if resp.status_code == 404:
             if not sb_create_services:
                 return
-
+            if not service.url:
+                service.url = service.name
             # first create the service with the Cabot ID as the name
-            s.post('%s/services' % (sb_alert_url),
+            a = s.post('%s/services' % (sb_alert_url),
                     data={
-                        #'name': key,
-                        'name': service.name,
+                        'name': key,
                         'description': service.url,
                     },
                     headers={'Content-Type':None}
                 )
+            f = open( '/tmp/testfile1', 'w' )
+            f.write( 'a.content = ' + a.content + '\n' )
+            f.close()
 
             # now change the name to the name in Cabot
-            s.post('%s/services/%s' % (sb_alert_url, service.name),
+            a = s.post('%s/services/%s' % (sb_alert_url, key),
                     data={
                         'name': service.name,
                         'description': service.url,
                     },
                     headers={'Content-Type':None}
                 )
+            f = open( '/tmp/testfile', 'w' )
+            f.write( 'b.content = ' + a.content + '\n' )
+            f.close()
 
         payload = {
                 'token':env['PUSHOVER_TOKEN'],
@@ -99,7 +107,7 @@ class StashboardAlert(AlertPlugin):
         }
 
         s.post(
-                '%s/services/%s/events' % (sb_alert_url, service.name),
+                '%s/services/%s/events' % (sb_alert_url, key),
                 data=payload,
                 headers={'Content-Type':None}
             )
